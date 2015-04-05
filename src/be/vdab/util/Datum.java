ced5d3bde7e177fe30c7df569e29b3b28c8cf055
Dieter Taillieu <dieter.taillieu@telenet.be>
2015-04-05 19:53:38 +0200

    complete Datum.java

--- src/be/vdab/util/Datum.java
+++ src/be/vdab/util/Datum.java
@@ -1,8 +1,10 @@
 package be.vdab.util;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
-public final class Datum {
+public final class Datum implements Comparable<Datum>, Serializable {
+
 	private final int dag;
 	private final int maand;
 	private final int jaar;
@@ -14,13 +16,49 @@ public final class Datum {
 		int[] m30 = {4,6,9,11};
 		if(Arrays.asList(m30).contains(maand) && dag>30) throw new DatumException();
 		if(maand==2) {
-			if(jaar%400==0 && dag>29) throw new DatumException();
-			else if(jaar%100==0 && dag>28) throw new DatumException();
-			else if(jaar%4==0 && dag>29) throw new DatumException();
-			else if(dag>28) throw new DatumException();
+			if(jaar%400==0) {
+				if(dag>29) throw new DatumException();
+			} else if(jaar%100==0) {
+				if(dag>28) throw new DatumException();
+			} else if(jaar%4==0) {
+				if(dag>29) throw new DatumException();
+			} else
+				if(dag>28) throw new DatumException();
 		}
 		this.dag = dag;
 		this.maand = maand;
 		this.jaar = jaar;
 	}
+
+	//
+
+	@Override
+	public int compareTo(Datum d) {
+		int result = this.getJaar() - d.getJaar();
+		if(result==0) result = this.getMaand() - d.getMaand();
+		if(result==0) result = this.getDag() - d.getDag();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof Datum) {
+			Datum d = (Datum) o;
+			if(this.getDag()==d.getDag() && this.getMaand()==d.getMaand() && this.getJaar()==d.getJaar()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int getDag() {return this.dag;}
+	public int getJaar() {return this.jaar;}
+	public int getMaand() {return this.maand;}
+
+	@Override
+	public int hashCode() {return String.format("%4d%02d%02d", this.jaar, this.maand, this.dag).hashCode();}
+
+	@Override
+	public String toString() {return String.format("%02d/%02d/%04d", this.dag, this.maand, this.jaar);}
+
 }
