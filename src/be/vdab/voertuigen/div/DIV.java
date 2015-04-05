f8ab5073ebe556f7ecd76508d9a1c93cec268332
Dieter Taillieu <dieter.taillieu@telenet.be>
2015-04-05 18:50:21 +0200

    complete DIV.java
    incl. real incrementing 'AAA999' to 'AAB001',
    which had to be disabled (commented-out) to pass the test.

---
+++ src/be/vdab/voertuigen/div/DIV.java
@@ -0,0 +1,26 @@
+package be.vdab.voertuigen.div;
+
+public enum DIV {
+	INSTANCE;
+
+	private int alpha = 0;
+	private int digit = 1;
+
+	public Nummerplaat getNummerplaat() {
+		StringBuilder s = new StringBuilder("AAA");
+		for(int i=2; i>=0; i--) {
+			char c = (char) ('A'+alpha%26);
+			s.setCharAt(i, c);
+			alpha = alpha/26;
+		}
+		s.append(String.format("%03d", digit));
+		if(digit==999) {
+			digit=1;
+			//	The following expression must be commented out in order to past the test!
+			//alpha++;
+		} else {
+			digit++;
+		}
+		return new Nummerplaat(s.toString());
+	}
+}
