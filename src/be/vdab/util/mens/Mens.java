05efed36db30ab3ee74cd2a8b3160c73afc3e661
Dieter Taillieu <dieter.taillieu@telenet.be>
2015-04-05 21:20:29 +0200

    complete Mens.java

---
+++ src/be/vdab/util/mens/Mens.java
@@ -0,0 +1,76 @@
+package be.vdab.util.mens;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class Mens implements Comparable<Mens> {
+
+	private String naam;
+	private Set<Rijbewijs> rijbewijs = EnumSet.noneOf(Rijbewijs.class);
+
+	//
+
+	public Mens(String naam) {
+		this.naam = naam;
+	}
+
+	public Mens(String naam, Rijbewijs r1, Rijbewijs... rest) {
+		this(naam);
+		this.rijbewijs = EnumSet.of(r1, rest);
+	}
+/*
+	public Mens(String naam, Rijbewijs r1) {}
+
+	public Mens(String naam, Rijbewijs r1, Rijbewijs r2) {}
+
+	public Mens(String naam, Rijbewijs r1, Rijbewijs r2, Rijbewijs r3) {}
+
+	public Mens(String naam, Rijbewijs r1, Rijbewijs r2, Rijbewijs r3, Rijbewijs r4) {}
+*/
+	//
+
+	@Override
+	public int compareTo(Mens m) {
+		int result = this.getNaam().compareTo(m.getNaam());
+		if(result==0) result = this.getRijbewijs().length - m.getRijbewijs().length;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(o!=null && o instanceof Mens) {
+			Mens m = (Mens) o;
+			if(this.getNaam().equals(m.getNaam())
+				&& this.getRijbewijs().length==m.getRijbewijs().length
+				&& this.getRijbewijs()[0].equals(m.getRijbewijs()[0])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getNaam() {return this.naam;}
+
+	public Object[] getRijbewijs() {return this.rijbewijs.toArray();}
+
+	public int hashCode() {return this.toString().hashCode();}
+
+	public String toString() {
+		StringBuilder s = new StringBuilder(this.naam);
+		if(! this.rijbewijs.isEmpty()) {
+			//s.append(this.getRijbewijs());
+			s.append("(");
+			int i=0;
+			for(Object o : this.rijbewijs) {
+				if(i==0) {
+					s.append(o);
+				} else {
+					s.append(", ").append(o);
+				}
+				i++;
+			}
+			s.append(")");
+		}
+		return s.toString();
+	}
+}
