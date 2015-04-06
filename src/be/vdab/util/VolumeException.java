815e7422797d4af0a8be07f9bee8b184f33ce380
Dieter Taillieu <dieter.taillieu@telenet.be>
2015-04-06 11:13:28 +0200

    complete VolumeException.java

---
+++ src/be/vdab/util/VolumeException.java
@@ -0,0 +1,13 @@
+package be.vdab.util;
+
+public class VolumeException extends Exception {
+
+	public VolumeException() {}
+
+	public VolumeException(String s) {super(s);}
+
+	public VolumeException(String s, Throwable e) {super(s, e);}
+
+	public VolumeException(Throwable e) {super(e);}
+
+}
