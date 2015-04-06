394d9cb94b23ff105170ecea005229fedab6233f
Dieter Taillieu <dieter.taillieu@telenet.be>
2015-04-06 10:50:08 +0200

    vervolg Voertuig.java
    TODO: abstract, validatie van bestuurders Rijbewijs.

--- src/be/vdab/voertuigen/Voertuig.java
+++ src/be/vdab/voertuigen/Voertuig.java
@@ -39,8 +39,23 @@ public class Voertuig implements Comparable<Voertuig>, Comparator<Voertuig>, Ser
 
 	//
 
-	public static Comparator<Voertuig> getAankoopprijsComparator() {}
-	public static Comparator<Voertuig> getMerkComparator() {}
+	public static Comparator<Voertuig> getAankoopprijsComparator() {
+		return new Comparator<Voertuig>() {
+			@Override
+			public int compare(Voertuig v1, Voertuig v2) {
+				return Integer.compare(v1.getAankoopprijs(), v2.getAankoopprijs());
+			}
+		};
+	}
+
+	public static Comparator<Voertuig> getMerkComparator() {
+		return new Comparator<Voertuig>() {
+			@Override
+			public int compare(Voertuig v1, Voertuig v2) {
+				return v1.getMerk().compareTo(v2.getMerk());
+			}
+		};
+	}
 
 	//
 
@@ -59,25 +74,54 @@ public class Voertuig implements Comparable<Voertuig>, Comparator<Voertuig>, Ser
 		return this.toString().compareTo(v.toString());
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof Voertuig) {
+			Voertuig v = (Voertuig) o;
+			return this.getNummerplaat().equals(v.getNummerplaat());
+		}
+		return false;
+	}
+
 	public int getAankoopprijs() {return this.aankoopprijs;}
+
 	public Mens getBestuurder() {return this.bestuurder;}
+
 	public Datum getDatumEersteIngebruikname() {return this.datumEersteIngebruikname;}
+
 	public TreeSet<Mens> getIngezeteneExclusiefBestuurder() {return this.passagiers;}
+
 	public TreeSet<Mens> getIngezetenen() {
 		TreeSet<Mens> allen = new TreeSet<Mens>(this.passagiers);
 		allen.add(this.bestuurder);
 		return allen;
 	}
+
 	public String getMerk() {return this.merk;}
+
 	public Nummerplaat getNummerplaat() {return this.nummerplaat;}
+
+	@Override
+	public int hashCode() {return getNummerplaat().hashCode();}
+
 	public boolean isIngezetene(Mens m) {return this.passagiers.contains(m) || this.bestuurder.equals(m);}
+
 	public void setAankoopprijs(int i) {this.aankoopprijs = i;}
 
 	public void setBestuurder(Mens m) {
 		if(!geldigeBestuurder(m)) throw new MensException("Kandidaat-bestuurder is onvoldoende gekwalificeerd.");
-		if(this.passagiers.contains(m)) this.passagiers.remove(m);
-		if(this.bestuurder!=null) addIngezetene(this.bestuurder);
-		this.bestuurder = m;
+		if(this.bestuurder==null) {// 				eerste bestuurder (kan enkel via de constructor)
+			this.bestuurder = m;
+		} else if(this.bestuurder.equals(m)) {// 	zelfde bestuurder
+			// geen actie vereist
+		} else if(this.passagiers.contains(m)) {//	wisseling passagier & bestuurder
+			this.passagiers.add(this.bestuurder);
+			this.bestuurder = m;
+		} else {//									nieuwe bestuurder
+			if(getIngezetenen().size()==this.MAX_ZITPLAATSEN) throw new MensException("Geen plaats voor een nieuwe bestuurder.");
+			this.passagiers.add(this.bestuurder);
+			this.bestuurder = m;
+		}
 	}
 
 	public void setDatumEersteIngebruikname(Datum d) {this.datumEersteIngebruikname = d;}
@@ -86,9 +130,12 @@ public class Voertuig implements Comparable<Voertuig>, Comparator<Voertuig>, Ser
 
 	@Override
 	public String toString() {
-		//StringBuilder s = new StringBuilder();
-
-		return String.format("%s %s %s %d %s %s", nummerplaat.toString(), merk, datumEersteIngebruikname.toString(), aankoopprijs, bestuurder.toString(), passagiers.toString());
+		StringBuilder s = new StringBuilder();
+		s.append( String.format("%s %s %s %d %s", nummerplaat.toString(), merk, datumEersteIngebruikname.toString(), aankoopprijs, bestuurder.toString()) );
+		if(getIngezeteneExclusiefBestuurder().size()>0) {
+			s.append(" "+passagiers.toString());
+		}
+		return s.toString();
 	}
 
 	//
